2945ea362c54fb94d14ba2ca932b4a2c92e39040
TanviSurve2005 <ts3956661@gmail.com>
2025-01-03 18:06:23 +0530

    TypeCasting_Subtypes

---
+++ ProblemSolving1.py
@@ -0,0 +1,32 @@
+name = input("Enter the Name of the person: ")
+age = int(input("Enter the Age of the person: "))
+address = input("Enter the Address of the person: ")
+
+print (name)
+print (age)
+print (address)
+
+a= 123
+b=25.2
+b= int(b)
+print(type(b))
+c=a+b
+print(c)
+
+p = int(input("Enter a Integer: "))
+print(p)
+print(type(p))
+p=float(p)
+print(type(p))
+
+x =12
+print("X is",x)
+y= 13
+print("Y is",y)
+
+temp = x
+print("Temp is",temp)
+x=y
+print("X is",x)
+y=temp
+print("Y is",y)
