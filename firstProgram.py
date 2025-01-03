19fa416f1d5d8e4e4cd11ef5f0d12fb5dca9b3fe
TanviSurve2005 <ts3956661@gmail.com>
2025-01-03 17:05:56 +0530

    Initial commit

---
+++ firstProgram.py
@@ -0,0 +1,20 @@
+print ("Hello")
+print("This is my first program")
+print ("I hope you all are good")
+print("""Hello 
+this is my first code""")
+print("hello world\nThis is my first code")
+#This is a comment
+"""This is a comments"""
+#Define inputs
+name = input("Enter your name: ")
+print(name)
+
+age = int(input("Enter your age :"))
+print(age)
+
+length = float(input("Enter your length :"))
+print(length)
+
+exp1 = eval(input("Enter any equation here: "))
+print(exp1)
